b2aff5bea344808acb036c1041844b4b43acc06a
Anuththara Gamage <anuththaragamage45@gmail.com>
2025-06-17 15:22:57 +0530

    Update SplineContainer scene URL in Index component for improved visual content.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1132,7 +1132,7 @@ const Index = () => {
           <div className="relative">
             <div className="relative">
               <div className="relative">
-                <SplineContainer scene="https://prod.spline.design/USMDn0jW6GUQEWn1/scene.splinecode" />
+                <SplineContainer scene="https://prod.spline.design/mcpJsVWWx6gkj3PL/scene.splinecode" />
                 <div className="absolute bottom-0 right-0 w-1/2 h-16 bg-[#000A33] z-10"></div>
               </div>
               <div className="text-center mt-12">
